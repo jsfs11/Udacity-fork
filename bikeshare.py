5ce44fb07b7ce68e0b226c7e83dc18f76639e893
James Wright <jsf.wright@gmail.com>
2022-05-03 20:46:16 +1000

    Significant refactoring included in previous commit, only a welcome message change :)

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
         str (month): name of the month to filter by, or "all" to apply no month filter
         str (day): name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Welcome! Let\'s explore some US bikeshare data!')
 
     city = ''
     while city not in CITY_DATA.keys():
